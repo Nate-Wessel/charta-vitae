69d0cb970c9729872397af6a07214cba91b94c3c
Nate-Wessel <bike756@gmail.com>
2019-05-16 12:13:30 -0400

    adding javascript to pages with shortcode

--- enable-shortcodes.php
+++ enable-shortcodes.php
@@ -23,4 +23,24 @@ function sitemap_shortcode_handler( $atts ){
 	return '';
 }
 add_shortcode( 'sitemap', 'sitemap_shortcode_handler' );
+
+# conditionally add javascript to header when shortcode found on page
+# thanks to: http://beerpla.net/2010/01/13/wordpress-plugin-development-how-to-include-css-and-javascript-conditionally-and-only-when-needed-by-the-posts/
+add_filter('the_posts', 'conditionally_add_scripts_and_styles'); // the_posts gets triggered before wp_head
+function conditionally_add_scripts_and_styles($posts){
+	if (empty($posts)) return $posts;
+	$shortcode_found = false; // use this flag to see if styles and scripts need to be enqueued
+	foreach ($posts as $post) {
+		if (stripos($post->post_content, '[sitemap]') !== false) {
+			$shortcode_found = true; // bingo!
+			break;
+		}
+	} 
+	if ($shortcode_found) {
+		// enqueue here
+		wp_enqueue_script('d3v5','https://d3js.org/d3.v5.js');
+		wp_enqueue_script('charta-vitae','/wp-content/plugins/charta-vitae/charta-vitae.js',array('d3v5'));
+	}
+	return $posts;
+}
 ?>
