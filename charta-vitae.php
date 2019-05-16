18e026207231e43832d37177d27f67f2e5f5880f
Nate-Wessel <bike756@gmail.com>
2019-05-15 20:35:30 -0400

    moved taxonomy creation

--- charta-vitae.php
+++ charta-vitae.php
@@ -13,31 +13,6 @@
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 include_once('enable-term-meta.php');
-
-# define custom taxonomy
-function occupation_init() {
-	// create a new taxonomy
-	register_taxonomy(
-		'occupation',
-		'post',
-		array(
-			'hierarchical' => false,
-			'show_ui' => true,
-			'show_in_rest' => true, # seems necessary to show in interface
-			'description' => 'things that have kept me busy',
-			'label' => __( 'Occupation' ),
-			'public' => false,
-			'rewrite' => false,
-			'capabilities' => array('manage_terms','edit_terms'),
-			'label' => 'occupations',
-			'labels' => array(
-				'name' => 'Occupations',
-				'singular_name' => 'Occupation',
-				'add_new_item' => 'Add new occupation'
-			),
-		)
-	);
-}
-add_action( 'init', 'occupation_init' );
+include_once('create-taxonomies.php')
 
 ?>
---
+++ create-taxonomies.php
@@ -0,0 +1,26 @@
+<?php
+# define custom taxonomies
+function occupation_init() {
+	register_taxonomy(
+		'occupation',
+		'post',
+		array(
+			'hierarchical' => false,
+			'show_ui' => true,
+			'show_in_rest' => true, # seems necessary to show in interface
+			'description' => 'things that have kept me busy',
+			'label' => __( 'Occupation' ),
+			'public' => false,
+			'rewrite' => false,
+			'capabilities' => array('manage_terms','edit_terms'),
+			'label' => 'occupations',
+			'labels' => array(
+				'name' => 'Occupations',
+				'singular_name' => 'Occupation',
+				'add_new_item' => 'Add new occupation'
+			),
+		)
+	);
+}
+add_action( 'init', 'occupation_init' );
+?>
