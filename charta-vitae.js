e1c1d0d07ea07cebcef90fe757e3e4dbc6898a95
Nate-Wessel <bike756@gmail.com>
2019-05-16 21:56:37 -0400

    graph colored by category

--- charta-vitae.js
+++ charta-vitae.js
@@ -1,20 +1,20 @@
 window.onload = function () {
 	// define vars
 	var nodes_data = [
-		{'id':'A','cat':'1'},
-		{'id':'B','cat':'1'},
-		{'id':'C','cat':'2'},
-		{'id':'D','cat':'2'},
-		{'id':'E','cat':'2'},
-		{'id':'F','cat':'2'},
-		{'id':'G','cat':'2'}
+		{'id':'A','cat':1},
+		{'id':'B','cat':1},
+		{'id':'C','cat':2},
+		{'id':'D','cat':2},
+		{'id':'E','cat':2},
+		{'id':'F','cat':2},
+		{'id':'G','cat':2}
 	]
 	var links_data = [
-		{'source':'A','target':'B','cat':'1'},
-		{'source':'B','target':'C','cat':'1'},
-		{'source':'D','target':'E','cat':'2'},
-		{'source':'E','target':'F','cat':'2'},
-		{'source':'F','target':'G','cat':'2'}
+		{'source':'A','target':'B','cat':1},
+		{'source':'B','target':'C','cat':1},
+		{'source':'D','target':'E','cat':2},
+		{'source':'E','target':'F','cat':2},
+		{'source':'F','target':'G','cat':2}
 	]
 
 	const width = 500
@@ -37,16 +37,18 @@ window.onload = function () {
 		.selectAll("line")
 		.data(links_data)
 		.enter().append("line")
-		.attr("stroke-width", 2);
+		.attr("stroke-width", 2)
+		.attr('stroke',function(d){ return d.cat==1 ? 'red' : 'blue' });
 
 	//draw circles for the nodes 
 	var node = svg.append("g")
 		.attr("class", "node")
-		.selectAll("circle")
+		.selectAll('circle')
 		.data(nodes_data)
 		.enter()
-		.append("circle")
-		.attr("r", 5);
+		.append('circle')
+		.attr('r', 10)
+		.attr('fill',function(d){ return d.cat==1 ? 'red' : 'blue' });
 
 	function tickActions() {
 		//update circle positions to reflect node updates on each tick of the simulation 
@@ -82,7 +84,16 @@ window.onload = function () {
 			d.fx = null;
 			d.fy = null;
 		})
-
 	//apply the drag_handler to our circles 
 	drag_handler(node);
+
+
+	// TODO:
+	// work on getting data from href links into an array
+	var posts = d3.select('#page').selectAll('a');
+	ps = posts;
+	console.log( ps, posts );
+
+
+
 }
