c58abf9d7dbc457a16c4d1ffacb53a120069c101
Nate-Wessel <bike756@gmail.com>
2019-05-21 15:40:03 -0400

    refactoring functiion defs

--- charta-vitae.js
+++ charta-vitae.js
@@ -32,7 +32,7 @@ function enable_data_changes(){
 			if(this.checked){
 				d3.selectAll('li.stratum[data-stratum='+stratumSlug+'] ul.fila')
 					.style('display','');
-				add_stratum(stratumSlug);
+				//add_stratum(stratumSlug);
 			}else{
 				d3.selectAll('li.stratum[data-stratum='+stratumSlug+'] ul.fila')
 					.style('display','none');
@@ -60,40 +60,31 @@ function get_data(){
 		}
 		if(elem.dataset.gemini){
 			for ( let targetId of elem.dataset.gemini.split(' ') ) {
-				links_data.push( {
-					'source':elem.dataset.nodeId, 'target':targetId, 'type':'geminus'
-				} );
+				if( targetId > elem.dataset.nodeId ) { // avoid duplicates
+					links_data.push( {
+						'source':elem.dataset.nodeId, 'target':targetId, 
+						'type':'geminus'
+					} );
+				}
 			}
 		}
 	}
 }
-/*
+
 function remove_stratum(slug){
-	// removes both nodes and links associated with this stratum
+	// first remove links, then nodes
+	// get a list of nodes to remove
+	let del_nodes = nodes_data.filter(node => node.stratum == slug);
+	// remove links associated with the nodes to remove
+	links_data = links_data.filter(
+		link => ( del_nodes.includes(link.target) || del_nodes.includes(link.source) )
+	);
+	// identify the nodes from this stratum and remove them
 	nodes_data = nodes_data.filter( node => node.stratum != slug );
-	// update the chart
-	var circles = d3.select('svg').selectAll('circle')
-		.data(nodes_data).exit().remove();
+	d3.select('svg').selectAll('circle').data(nodes_data).exit().remove();
+	d3.select('svg').selectAll('line').data(links_data).exit().remove();
 }
 
-function add_stratum(slug){
-	// adds both nodes and links associated with this stratum
-	nodes = d3.selectAll('#page li.eventus[data-stratum='+slug+']').nodes();
-	for ( let elem of nodes ) {
-		nodes_data.push({
-			'id':elem.dataset.nodeId, 'stratum':slug,
-			'filum':elem.dataset.filum, 
-			'url':d3.select(elem).select('a').attr('href')
-		})
-	}
-	var circles = d3.select('svg').selectAll('circle')
-		.data(nodes_data).enter().append('svg:a')
-		.attr('xlink:href',function(d){return d.url;})
-		.append('circle').attr('r',radius).attr('fill','gray');
-
-	if (!d3.event.active) sim.alphaTarget(0.3).restart()
-}
-*/
 function create_graph(svg){
 	link_stroke = function(data){
 		return data.type=='filum' ? 'black' : 'red';
@@ -117,7 +108,7 @@ function create_graph(svg){
 		.strength(-20)
 
 	// attach the forces
-	var sim = d3.forceSimulation().nodes(nodes_data)
+	var simulation = d3.forceSimulation().nodes(nodes_data)
 		.force('charge_force',staticForce)
 		.force('center_force',d3.forceCenter(width/2, height/2))
 		.force('link_force',linkForce)
@@ -127,39 +118,35 @@ function create_graph(svg){
 	var link = svg.append("g")
 		.attr('id','edges')
 		.selectAll("line").data(links_data).enter().append("line")
-		.attr('stroke',function(d){return link_stroke(d)})
-		.attr('class',function(d){return d.type});
+		.attr('stroke',d => link_stroke(d))
+		.attr('class', d => d.type );
 
 	//draw circles for the nodes, where each is a link to the post
 	var nodes = svg.append("g").attr('id','nodes')
 		.selectAll('circle')
 		.data(nodes_data).enter()
-		.append('svg:a').attr('xlink:href',function(d){return d.url;})
-		.append('circle').attr('r', radius).attr('fill','gray');
+		.append('svg:a')
+		.attr( 'xlink:href', d => d.url )
+		.append('circle')
+		.attr('r', radius)
+		.attr('fill','gray');
 
-
+	// called on each simulation tick - updates geometry positions
+	simulation.on("tick", tickActions );
 	function tickActions() {
 		//update circle positions to reflect node updates on each tick of the simulation 
-		nodes
-			.attr("cx", function(d) { return d.x; })
-			.attr("cy", function(d) { return d.y; })
+		nodes.attr("cx", d => d.x ).attr("cy", d => d.y );
 		//update link positions 
-		//simply tells one end of the line to follow one node around
-		//and the other end of the line to follow the other node around
 		link
-			.attr("x1", function(d) { return d.source.x; })
-			.attr("y1", function(d) { return d.source.y; })
-			.attr("x2", function(d) { return d.target.x; })
-			.attr("y2", function(d) { return d.target.y; });
+			.attr("x1", d => d.source.x ).attr("y1", d => d.source.y )
+			.attr("x2", d => d.target.x ).attr("y2", d => d.target.y );
 	}
-	// assign this to listen to events
-	sim.on("tick", tickActions );
 
 	//create drag handler     
 	var drag_handler = d3.drag()
 		.on('start', function(d){
 			// We set the fixed position of the dragged node to be wherever it was when we clicked
-			if (!d3.event.active) sim.alphaTarget(0.3).restart();
+			if (!d3.event.active) simulation.alphaTarget(0.3).restart();
 			d.fx = d.x;
 			d.fy = d.y;
 		})
@@ -168,7 +155,7 @@ function create_graph(svg){
 			d.fy = d3.event.y;
 		})
 		.on('end',function(d){
-			if (!d3.event.active) sim.alphaTarget(0);
+			if (!d3.event.active) simulation.alphaTarget(0);
 			d.fx = null;
 			d.fy = null;
 		})
