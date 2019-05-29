ec471a790cf96562610db9cac907402e4fdd9caa
Nate-Wessel <bike756@gmail.com>
2019-05-29 16:11:31 -0400

    enabled strata metadata for display default

--- create-taxonomies.php
+++ create-taxonomies.php
@@ -23,4 +23,48 @@ function stratum_init() {
 	);
 }
 add_action( 'init', 'stratum_init' );
+
+// add term metadata ( display by default checkbox ) to editing forms 
+
+function add_stratum_display_field($term) {
+	$meta = get_term_meta($term->term_id,'display');
+	$checked = (count($meta)>0 && $meta[0]=='true') ? true : false;
+	?>
+	<div>
+		<input type="checkbox" name="display" value="true" <?php if($checked) echo 'checked';?>>
+		<label>Display by default?</label>
+	</div><?php
+}
+add_action( 'strata_edit_form_fields', 'add_stratum_display_field' );
+
+
+// save that checkbox value in the DB
+function save_stratum_display_field( $term_id ){
+	$val = $_POST['display'];
+	if($val){
+		update_term_meta( $term_id, 'display', $val );
+	}else{
+		delete_term_meta( $term_id, 'display' );
+	}
+}
+add_action( 'edited_strata', 'save_stratum_display_field' );
+
+
+
+// these two add a column to the table 
+function add_display_column( $columns ){
+    $columns['display'] = 'Display';
+    return $columns;
+}
+add_filter('manage_edit-strata_columns', 'add_display_column' );
+
+function add_display_column_content( $content, $column_name, $term_id ){
+	if( $column_name !== 'display' ){ return $content; }
+	$term_id = absint( $term_id );
+	$displayValue = get_term_meta( $term_id, 'display', true );
+	$content .= $displayValue;
+	return $content;
+}
+add_filter('manage_strata_custom_column', 'add_display_column_content', 10, 3 );
+
 ?>
