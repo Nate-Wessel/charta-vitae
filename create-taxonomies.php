f62edadb03984aa73033f9851899e84a3bf37c0b
Nate-Wessel <bike756@gmail.com>
2019-07-14 10:10:45 -0400

    added tags to cv events

--- create-taxonomies.php
+++ create-taxonomies.php
@@ -1,5 +1,31 @@
 <?php
 # define custom taxonomies
+
+function CV_event_tags_init() {
+	register_taxonomy(
+		'CV_event_tag',
+		array('cv_event'),
+		array(
+			'hierarchical' => false,
+			'show_ui' => true,
+			'show_in_rest' => true, # seems necessary to show in interface
+			'description' => 'Key=Value tags associated with life events',
+			'label' => __( 'tags' ),
+			'public' => false,
+			'rewrite' => false,
+			'capabilities' => array('manage_terms','edit_terms'),
+			'label' => 'Tag',
+			'labels' => array(
+				'name' => 'Tags',
+				'singular_name' => 'Tag',
+				'add_new_item' => 'Add new tag'
+			),
+		)
+	);
+}
+add_action( 'init', 'CV_event_tags_init' );
+
+
 function stratum_init() {
 	register_taxonomy(
 		'strata',
