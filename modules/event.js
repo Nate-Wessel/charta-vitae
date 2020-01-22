db938ad1100e35064d92e28c00fa72fdfb82e640
Nate Wessel <bike756@gmail.com>
2020-01-22 10:08:36 -0500

    make time handing more robust

--- modules/event.js
+++ modules/event.js
@@ -3,10 +3,9 @@ class CVevent {
 		this._id = id; // WP post ID
 		this._url = url; // WP post href
 		this._title = title; 
-		this._times = [ 
-			new CVtimePoint(start), 
-			new CVtimePoint(end) 
-		];
+		this._times = [];
+		if(start){ this._times.push( new CVtimePoint(start) ); } 
+		if(end){ this._times.push( new CVtimePoint(end) ); } 
 		this.strata = strata; 
 		this.tags = tags;
 		// reserved for simulation
@@ -17,8 +16,16 @@ class CVevent {
 	get id(){ return this._id; } // WP post_id
 	get url(){ return this._url; }
 	get title(){ return this._title; }
-	get start(){ return this._times[0].etime; }
-	get end(){ return this._times[1].etime; }
+	get start(){ 
+		if( this._times.length > 0 ){
+			return this._times[0].etime; 
+		}
+	}
+	get end(){ 
+		if( this._times.length > 0 ){
+			return this._times[this._times.length-1].etime; 
+		}
+	}
 	get duration(){ 
 		// estimated duration of event in seconds, defaulting to 0
 		if ( this.start && this.end  && this.start <= this.end ) {
--- modules/timepoint.js
+++ modules/timepoint.js
@@ -1,11 +1,12 @@
 class CVtimePoint{
 	// A point in time, measured with more or less accuracy
 	constructor(timeString){
+		console.assert(typeof(timeString)=='string');
 		this._time_string = timeString;
 		this._unix_time = cvDateParse(timeString);
+		//this._certainty = 1 / ( 10 - timeString.length );
 		// reserved for simulation
 		this.x; this.y; this.vx; this.vy;
-		//this._certainty = 1/(10- timeString.length);
 	}
 	get etime(){ return this._unix_time; }
 }
