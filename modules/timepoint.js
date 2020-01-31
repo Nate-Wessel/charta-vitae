c51ea67d33a762573200643a62211203e3bd71f2
Nate Wessel <bike756@gmail.com>
2020-01-31 16:28:56 -0500

    refactor timepoint

--- modules/timepoint.js
+++ modules/timepoint.js
@@ -1,20 +1,38 @@
 class CVtimePoint{
-	// A point in time, measured with more or less accuracy
-	constructor(timeString,parentEvent){
-		this.parent = parentEvent;
-		console.assert(typeof(timeString)=='string');
+	// A point in time, measured with variable precision
+	constructor( timeString, parent ){
+		// link to parent project/event
+		console.assert( parent instanceof CVevent );
+		this.parent = parent;
+		// parse the time once on construction
+		console.assert( typeof(timeString) == 'string' );
 		this._time_string = timeString;
 		this._unix_time = cvDateParse(timeString);
-		//this._certainty = 1 / ( 10 - timeString.length );
+		// TODO improve precision measure
+		this._precison = timeString.length;
 		// reserved for simulation
 		this.x; this.y; this.vx; this.vy;
 	}
-	get etime(){ return this._unix_time; }
-	get id(){ return this.parent.id+'|'+this._time_string; }
-	get tags(){ return this.parent.tags }
-	get radius(){ return 8; }
-	get url(){ return this.parent.url; }
-	get title(){ return this.parent.title; }
-	get optimalY(){ return e2y(this.etime); }
+	get etime(){ 
+		return this._unix_time; 
+	}
+	get id(){ 
+		return this.parent.id+'|'+this._time_string; 
+	}
+	get tags(){ 
+		return this.parent.tags 
+	}
+	get radius(){ 
+		return 8; 
+	}
+	get url(){
+		return this.parent.url;
+	}
+	get title(){
+		return this.parent.title;
+	}
+	get optimalY(){ 
+		return e2y(this.etime); 
+	}
 }
 
