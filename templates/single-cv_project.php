7fbac9b505dd0d4725847a25007f7b7c2c826cb8
Nate Wessel <bike756@gmail.com>
2020-02-09 17:05:08 -0500

    print dates more neatly

--- templates/single-cv_project.php
+++ templates/single-cv_project.php
@@ -3,18 +3,32 @@
 	<h1><?php echo get_the_title(); ?></h1>
 	<!--TODO add proper dynamic link-->
 	<p><a href="<?php echo get_site_url();?>/?page_id=1285">Chartae redi</a></p>
-	<?php if(have_posts()){ the_post();
+	<?php if(have_posts()){ 
+		the_post();
+		// query all the metadata!
 		$start = get_post_meta($post->ID,'start',true);
 		$end = get_post_meta($post->ID,'end',true);
-		echo "<p>Starts: $start, Ends: $end</p>"; ?>
-		<?php the_content(); ?>
-		<div id="metabox">
-		<?php $tags = get_the_terms($post->ID,'cv_tag');
+		$tags = get_the_terms($post->ID,'cv_tag');
+		$components = [];
+		$parent = [];
+		// print start/end dates neatly
+		if($start and $end and $start != $end){
+			echo "<p>Started $start and ended $end</p>";
+		}elseif($start and !$end){
+			echo "<p>Started: $start and still going</p>";
+		}elseif($end and !$start or $start == $end ){
+			echo "<p>Happened on $end</p>";
+		}else{
+			echo "<p>failed to parse '$start', '$end'</p>";
+		}
+		// print page content
+		the_content();
+		// print project tags
+		echo '<div id="metabox">';
 		if($tags){ foreach( $tags as $tag){
 				echo "<span class='tag'>$tag->name</span>";
-		} } ?>
-		</div><!--#metabox-->
-		<?php
+		} }
+		echo '</div><!--#metabox-->';
 	} // end the loop 
 	?>
 </div><!--#charta-vitae-->
