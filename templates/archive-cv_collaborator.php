5d44c70171b0970ab973fbf1e65878c1a6d036b5
Nate Wessel <bike756@gmail.com>
2020-02-10 20:10:41 -0500

    enable people archive

--- templates/archive-cv_collaborator.php
+++ templates/archive-cv_collaborator.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
 
-<p>cv_collaborator archive</p>
+<h1>Collaborator archive</h1>
+
+<?php
+// the query
+$wpq = new WP_Query(array('post_type'=>'cv_collaborator', 'posts_per_page'=>-1));
+ 
+if ( $wpq->have_posts() ) { 
+?>
+<ul>
+	<?php while ( $wpq->have_posts() ){ $wpq->the_post(); ?>
+		<li><a href="<?php the_permalink();?>"><?php the_title();?></a></li>
+	<?php } ?>
+</ul>
+
+<?php } ?>
 
 <?php get_footer(); ?>
