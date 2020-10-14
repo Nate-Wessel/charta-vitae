1d2f71405c435a067a96d8c224fb41d122b127a8
Nate Wessel <bike756@gmail.com>
2020-10-14 19:54:38 -0400

    light refactor

--- charta-vitae.php
+++ charta-vitae.php
@@ -12,32 +12,42 @@
 # prevent abuse:
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+$cv_dir = 'wp-content/plugins/charta-vitae';
+
 include_once('entities/projects.php');
 include_once('entities/collaborators.php');
 include_once('entities/tags.php');
 include_once('enable-shortcodes.php');
 include_once('JSON-API.php');
 
-add_filter( 'template_include', 'cv_templates' );
-function cv_templates( $template ) {
-	$plugin = 'wp-content/plugins/charta-vitae';
-	if( is_singular() && get_post_type() == 'cv_project' ){
-		wp_enqueue_style('CVstyle',"/$plugin/charta.css");
-		return "$plugin/templates/single-cv_project.php";
-	}elseif( is_singular() && get_post_type() == 'cv_collaborator' ){
-		wp_enqueue_style('CVstyle',"/$plugin/charta.css");
-		return "$plugin/templates/single-cv_collaborator.php";
-	}elseif( get_post_type() == 'cv_project' ){
-		wp_enqueue_style('CVstyle',"/$plugin/charta.css");
-		return "$plugin/templates/archive-cv_project.php";
-	}elseif( get_post_type() == 'cv_collaborator' ){
-		wp_enqueue_style('CVstyle',"/$plugin/charta.css");
-		return "$plugin/templates/archive-cv_collaborator.php";
+add_filter( 'template_include', 'cv_project_templates' );
+add_filter( 'template_include', 'cv_collaborator_templates' );
+add_action( 'pre_get_posts', 'cv_change_sort_order'); 
+
+function cv_project_templates( $template ) {
+	global $cv_dir;
+	if( get_post_type() == 'cv_project' ){
+		wp_enqueue_style('CVstyle',"/$cv_dir/charta.css");
+		if( is_singular() ){ 
+			return "$cv_dir/templates/single-cv_project.php";
+		}
+		return "$cv_dir/templates/archive-cv_project.php";
 	}
 	return $template;
 }
 
-add_action( 'pre_get_posts', 'cv_change_sort_order'); 
+function cv_collaborator_templates( $template ) {
+	global $cv_dir;
+	if( get_post_type() == 'cv_collaborator' ){
+		wp_enqueue_style('CVstyle',"/$cv_dir/charta.css");
+		if( is_singular() ){ 
+			return "$cv_dir/templates/single-cv_collaborator.php";
+		}
+		return "$cv_dir/templates/archive-cv_collaborator.php";
+	}
+	return $template;
+}
+
 function cv_change_sort_order($query){ 
 	// change sort order of archive pages listing projects
 	if( 
