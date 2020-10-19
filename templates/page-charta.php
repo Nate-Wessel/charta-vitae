014deea8cc3ee387524aeb1086573113a778efc2
Nate Wessel <bike756@gmail.com>
2020-10-18 20:53:41 -0400

    create simple embed page

--- templates/page-charta.php
+++ templates/page-charta.php
@@ -1,1 +1,14 @@
-yohoho this be the page!
+<?php $plugin_dir = '/wp-content/plugins/charta-vitae'; ?>
+<!DOCTYPE html>
+<html id="cv">
+	<head>
+		<title>Charta Vitae</title>
+		<meta charset='utf-8'/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="<?php echo site_url("$plugin_dir/charta.css")?>"/>
+		<script src="<?php echo site_url("$plugin_dir/bundle.js")?>"/></script>
+	</head>
+	<body>
+		<div id='charta-vitae'></div>
+	</body>
+</html>
